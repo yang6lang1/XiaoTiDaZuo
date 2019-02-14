45b24b64230be95ecc087d9223036e7fdae913df
Alex Yang <guang.alex.yang@gmail.com>
2019-02-13 23:32:26 -0800

    Start working on app Login logic
    - Finished ReactJS tutorial
    - Studied on some of the MaterialUI + ReactJS login page examples
    - Setup project to use:
    react, materialUI, babel, css-loader, etc

---
+++ client/src/authentication/LoginForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import './resources/LoginForm.css';
+
+function LoginForm(props) {
+    return (<Typography className="LoginPageHeader" variant="h4" gutterBottom>
+        小蹄大做
+    </Typography>);
+}
+
+export default LoginForm;
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import LoginForm from './authentication/LoginForm';
+
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
