656b5ca8ddc632f53ee40838a4ec053f3d1be798
Alex Yang <guang.alex.yang@gmail.com>
2019-02-20 01:26:48 -0800

    Working on LoginForm UI
    - Learned MaterialUI's Typograph, TextField, Button
    - Moving on to hooking up redux store with react component

--- client/src/authentication/LoginForm.js
+++ client/src/authentication/LoginForm.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import './resources/LoginForm.css';
 
 function LoginForm(props) {
-    return (<Typography className="LoginPageHeader" variant="h4" gutterBottom>
-        小蹄大做
-    </Typography>);
+    return (
+        <div className="LoginForm">
+            <div className="LoginFormSection">
+                <Typography className="LoginFormTitle" variant="h4" gutterBottom>
+                    小蹄大做
+                </Typography>
+                <Typography className="LoginFormSubtitle" variant="h6" gutterBottom>
+                    登录
+                </Typography>
+                <div className="LoginFormInputArea">
+                    <TextField className="LoginFormUserNameField" label="账号" type="text"/>
+                    <TextField className="LoginFormPasswordField" label="密码" type="password"/>
+                </div>
+                <div className="LoginFormInputButtonField">
+                    <Button variant="contained" color="primary" className="LoginFormLoginButton">
+                        登录
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LoginForm;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import LoginForm from './authentication/LoginForm';
+import './index.css';
 
 ReactDOM.render(<LoginForm />, document.getElementById('root'));
